688a98b9408fd455700ee3dad90f7ec1ce0f2a91
Jorge-jr <jsribeirojr@hotmail.com>
2023-03-04 19:40:29 -0300

    adding hamming.py unit tests

--- Python/hamming.py
+++ Python/hamming.py
@@ -1,30 +1,45 @@
 def encoder(data, full_size=7, data_size=4):
-    
     data = list(data)
     if len(data) % data_size != 0:
-        data += [0] * (len(data) % data_size)
-    encoded_data = [[None] * full_size] * (len(data)//data_size)
+        data += ["0"] * (data_size - (len(data) % data_size))
+    encoded_data_template = [[None] * full_size] * (len(data)//data_size)
     parity_indexes = [(2**x)-1 for x in range(full_size-data_size)]
-    for index, packet in enumerate(encoded_data):
-        for i in range(full_size-data_size):  # this loop fills with 0's all the parity bit positions
-            packet[(2**i)-1] = "0"
-        for j in range(len(packet)):  # this loop places the data bits on its positions
+    encoded_data = []
+    for index, block in enumerate(encoded_data_template):
+        encoded_data_line = block[:]
+        for i in range(full_size-data_size):
+            # this loop fills with 0's all the parity positions
+            encoded_data_line[(2**i)-1] = "0"
+        for j in range(len(block)):
+            # this loop places the data bits on its positions
             if j not in parity_indexes:
-                encoded_data[index][j] = data.pop(0)
+                encoded_data_line[j] = data.pop(0)
+        encoded_data += [encoded_data_line]
     result = []
-    for packet in encoded_data:
-        result += fill_parity(packet)
 
-    return result
+    for block in encoded_data:
+        result += fill_parity(block)
+
+    return "".join(result)
 
 
-def check(data, parity_size=3):
+def check(data, full_size=7, data_size=4):
+    if len(data) % full_size != 0:
+        raise ValueError("The 'data' size doesn't match the given 'full_size'")
+    result = []
+    for block_index in range(0, len(data), full_size):
+        result += [check_block(data[block_index:block_index+full_size])]
+    return "".join(result)
+
+
+def check_block(data, full_size=7, data_size=4):
     data = list(data)
-    parity_indexes = [(2**x) for x in range(parity_size)]
+    parity_indexes = [(2**x) for x in range(full_size-data_size)]
     parity_checker = 0b1
-    indexes_with_errors = []   
+    indexes_with_errors = []
     for parity_index in parity_indexes:
-        check_indexes = [x-1 for x in range(parity_index, len(data)+1) if parity_checker & x != 0]
+        check_indexes = [x-1 for x in range(parity_index, len(data)+1) if
+                         parity_checker & x != 0]
         check_sum = 0
         for check_index in check_indexes:
             check_sum += int(data[check_index])
@@ -33,7 +48,8 @@ def check(data, parity_size=3):
         parity_checker = parity_checker << 1
 
     if 1 in indexes_with_errors:
-        error_index = sum([bit*(2**index) for index, bit in enumerate(indexes_with_errors)]) - 1
+        error_index = sum([bit*(2**index) for index, bit in enumerate(
+            indexes_with_errors)]) - 1
         if data[error_index] == '0':
             data[error_index] = "1"
         else:
@@ -47,7 +63,8 @@ def fill_parity(data, parity_size=3):
     parity_indexes = [(2**x) for x in range(parity_size)]
     parity_checker = 0b1
     for parity_index in parity_indexes:
-        check_indexes = [x-1 for x in range(parity_index, len(data)+1) if parity_checker & x != 0]
+        check_indexes = [x-1 for x in range(parity_index, len(data)+1) if
+                         parity_checker & x != 0]
         check_sum = 0
         for check_index in check_indexes:
             check_sum += int(data[check_index])
@@ -57,18 +74,16 @@ def fill_parity(data, parity_size=3):
     return data
 
 
-def decoder(packet, full_size=7, data_size=4):
+def decoder(data, full_size=7, data_size=4):
 
-    if len(packet) % full_size != 0:
-        raise ValueError("Packet size error")
-    
-    data_parity = []
-
-    for chunk in range(0, len(packet), full_size):
-        data_parity += [(packet[chunk:chunk+data_size], packet[chunk+data_size:chunk+full_size])]
-    
-    print(data_parity)
-
-
-print(encoder("110011001100"))
-print(check("0111000")=="0111100")
+    if len(data) % full_size != 0:
+        raise ValueError("block size error")
+    corrected = check(data, full_size, data_size)
+    parity_indexes = [(2**x)-1 for x in range(full_size-data_size)]
+    result = ""
+    for block_index in range(0, len(corrected), full_size):
+        for bit_index, bit in enumerate(
+                corrected[block_index:block_index+full_size]):
+            if bit_index not in parity_indexes:
+                result += bit
+    return result
---
+++ Python/test_hamming.py
@@ -0,0 +1,53 @@
+import unittest
+from hamming import encoder, decoder, check
+
+
+"""
+    encoder 7, 4
+
+    000100000101 -> 110100100000000100101  -- multiple blocks, correct size
+    0001 -> 1101001  --  single block, correct size
+    00011 -> 11010011110000   -- single block, wrong size
+    00011001100101001101010101 -> 1101001001100100110011001100101010101001011001100 -- multiple blocks, wrong size
+
+
+    check 7, 4
+
+    1101001 0001000 0100101 -> 1101001 0000000 0100101  -- multiple blocks, 1 wrong block
+    1001001 -> 1101001  --  single block
+    11010001110001 -> 11010011110000   -- 2 wrong blocks
+    1101101001110100100011011100100010101101011001000 -> 1101001001100100110011001100101010101001011001100 -- multiple wrong blocks
+
+
+    decoder 7, 4
+
+    110100100000000100101 -> 000100000101
+    1101001 -> 0001
+    11010011110000 -> 00011000
+    1101001001100100110011001100101010101001011001100 -> 0001100110010100110101010100
+
+"""
+
+
+class TestHamming(unittest.TestCase):
+    def test_encoder(self):
+        self.assertEqual(encoder('0001'), "1101001")
+        self.assertEqual(encoder('000100000101'), "110100100000000100101")
+        self.assertEqual(encoder('00011'), "11010011110000")
+        self.assertEqual(encoder('00011001100101001101010101'), "1101001001100100110011001100101010101001011001100")
+
+    def test_check(self):
+        self.assertEqual(check('110100100010000100101'), "110100100000000100101")
+        self.assertEqual(check('1001001'), "1101001")
+        self.assertEqual(check('11010001110001'), "11010011110000")
+        self.assertEqual(check('1101101001110100100011011100100010101101011001000'), "1101001001100100110011001100101010101001011001100")
+
+    def test_decoder(self):
+        self.assertEqual(decoder('110100100000000100101'), "000100000101")
+        self.assertEqual(decoder('1101001'), "0001")
+        self.assertEqual(decoder('11010011110000'), "00011000")
+        self.assertEqual(decoder('1101001001100100110011001100101010101001011001100'), "0001100110010100110101010100")
+
+
+if __name__ == '__name__':
+    unittest.main()
