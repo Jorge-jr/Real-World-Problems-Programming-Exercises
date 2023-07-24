761dd487f285f89666a1fe84637aeb9c12ad2c77
Jorge-jr <jsribeirojr@hotmail.com>
2023-07-24 09:36:01 -0300

    Update functional_sudoku.ex
    Added a moduledoc

--- Elixir/exercises/lib/functional_sudoku.ex
+++ Elixir/exercises/lib/functional_sudoku.ex
@@ -1,32 +1,23 @@
 defmodule Exercises.FunctionalSudoku do
   @moduledoc """
-  Documentation for `Exercises`.
-  """
-
-  @doc """
-  Hello world.
+  This module provides functions to solve Sudoku puzzles using functional programming. Aditionally, providing an empty board will create a new one to be solved.
 
   ## Examples
 
-      iex> Exercises.hello()
-      :world
-
-  """
-  @empty_board [
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-
-  ]
-    # import Exercises.FunctionalSudoku
-
-
+  ```elixir
+  iex> board = [
+  ...>   [5, 3, nil, nil, 7, nil, nil, nil, nil],
+  ...>   [6, nil, nil, 1, 9, 5, nil, nil, nil],
+  ...>   [nil, 9, 8, nil, nil, nil, nil, 6, nil],
+  ...>   [8, nil, nil, nil, 6, nil, nil, nil, 3],
+  ...>   [4, nil, nil, 8, nil, 3, nil, nil, 1],
+  ...>   [7, nil, nil, nil, 2, nil, nil, nil, 6],
+  ...>   [nil, 6, nil, nil, nil, nil, 2, 8, nil],
+  ...>   [nil, nil, nil, 4, 1, 9, nil, nil, 5],
+  ...>   [nil, nil, nil, nil, 8, nil, nil, 7, 9]
+  ...> ]
+  ...> Exercises.FunctionalSudoku.fill_board(board)
+"""
 
   def fill_board(board\\@empty_board), do: do_fill_board(board, {0, 0})
 
